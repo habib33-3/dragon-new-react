1f1144a36311090441532cd64ea66d7239dd5945
Habibur Rahman <habib.swi16@gmail.com>
2023-10-02 23:21:22 +0600

    left side navigation

--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
   return (
     <div>
       <div>
-     <p>left</p>
+        <h2 className="text-2xl">All Categories</h2>
+        <div className="space-y-6">
+          {categories.map((category) => (
+            <Link
+              key={category.id}
+              className="block ml-4 text-xl font-semibold"
+              to={`/category/${category.id}`}
+            >
+              {category.name}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
